e81834f99744da762536a07eda23f21950e5cb68
David Domínguez <50904238+daviddominguez-ddl@users.noreply.github.com>
2019-10-24 00:05:21 -0500

    Update geometric_transform.cpp

--- Geometric-Transformations/geometric_transform.cpp
+++ Geometric-Transformations/geometric_transform.cpp
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2019, David Dominguez
+ * david.dominguez1603@gmail.com
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification,
